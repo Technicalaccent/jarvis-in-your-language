9379bc6638ea3995e9a9666d53625a5d8eed9acd
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-07-12 13:25:41 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ sudo apt install python3-pyaudio -y
 sudo apt install flac mpg321 -y
 pip3 install gtts speechrecognition
 pip3 install -U textblob
+pip3 install wikipedia
 sudo python3 -m textblob.download_corpora
